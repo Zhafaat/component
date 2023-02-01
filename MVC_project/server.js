357b8080694f0d6ef228aabcf7d96baa87d119e2
Zhafaat <ilmuumum18@gmail.com>
2023-02-01 22:41:25 +0800

    request dan response

--- MVC_project/server.js
+++ MVC_project/server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('request made');
+    console.log(req.url, req.method, req);
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    res.write('<head><link rel="styleseet" href="#"></head>');
+    res.write('<p>hello, zhafaat</p>');
+    res.write('<p>hello again, zhafaat</p>');
+    res.end();
 });
 
 server.listen(3000, 'localhost', () => {
