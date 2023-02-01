bf1929366561e3fc3a0ee0c4b1a52ffd94575364
Zhafaat <ilmuumum18@gmail.com>
2023-02-01 23:21:54 +0800

    returning html file

--- MVC_project/server.js
+++ MVC_project/server.js
@@ -1,15 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req);
+    console.log(req.url, req.method);
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<head><link rel="styleseet" href="#"></head>');
-    res.write('<p>hello, zhafaat</p>');
-    res.write('<p>hello again, zhafaat</p>');
-    res.end();
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err) {
+            console.log(err);
+            res.end();
+        } else {
+            // res.write(data);
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3000, 'localhost', () => {
